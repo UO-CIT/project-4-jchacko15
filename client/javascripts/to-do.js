19f3dfe6457ccc8129bd82c3d3800e9072f57b9f
jchacko15 <63620708+jchacko15@users.noreply.github.com>
2020-05-26 15:40:57 -0700

    Update to-do.js

--- client/javascripts/to-do.js
+++ client/javascripts/to-do.js
@@ -1,4 +1,9 @@
 //jshint esversion: 6
+if(localStorage.getItem("toDoList") != null ){
+$(".comments").html(localStorage.getItem("toDoList"));
+}
+localStorage.clear();
+
 
 let controller = function() {
 
@@ -10,20 +15,72 @@ let controller = function() {
       $new_comment = $("<p>").text($(".comment-input input").val());
       //$new_comment.hide();
       $(".comments").append($new_comment);
-      //$new_comment.fadeIn();
-      $(".comment-input input").val("");
+
+
+
+
+      console.log($(".comments").html());
+      if (typeof(Storage) !== "undefined") {
+      // Code for localStorage/sessionStorage.
+      //localStorage.removeItem$(".comments");
+
+      } else {
+        //localStorage.removeItem$(".comments");
+        // Sorry! No Web Storage support..
+      }
+      localStorage.setItem("toDoList",$(".comments").html());
+
     }
   };
+
 
   $(".comment-input button").on("click", function(event) {
     addCommentFromInputBox();
   });
 
-  $(".comment-input input").on("keypress", function(event) {
-    if (event.keyCode === 13) {
-      addCommentFromInputBox();
-    }
-  });
 };
 
 $(document).ready(controller);
+function appendItems() {
+//  alert("test1");
+  let $new_comment;
+  //$new_comment = "JohnTest";
+  //$(".comments").append($new_comment);
+  if (typeof(Storage) !== "undefined")
+  {
+  //  alert("test2");
+  // Code for localStorage/sessionStorage.
+    /*
+    let key = localStorage.key(i);
+    alert(`${key}: ${localStorage.getItem(key)}`);
+
+   let keys = Object.keys(localStorage);
+    for(let key of keys)
+    {
+      alert(`${key}: ${localStorage.getItem(key)}`);
+    }
+*/
+//localStorage.removeItem($(".comments"));
+//localStorage.clear();
+    for (var i = 0; i < localStorage.length; i++)
+    {
+    //  alert("test4");
+      //alert(localStorage.length);
+      let key = localStorage.key(i);
+      //alert(`${key}: ${localStorage.getItem(key)}`);
+      $new_comment = localStorage.key(i);
+    $(".comments").append($new_comment);
+    //$(".comments").html(localStorage.getItem("toDoList"));
+    //  alert(i);
+    }
+
+  }
+  else
+  {
+  // Sorry! No Web Storage support..
+//alert("test3");
+  //$new_comment = "JohnTest";
+  //$(".comments").append($new_comment);
+  }
+}
+//alert(`${key}: ${localStorage.getItem(key)});
